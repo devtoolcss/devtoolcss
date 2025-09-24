4313249e778ea64bf38ba0d3f6cefd4a5dd13cb3
Bill Tsui <b10902118@ntu.edu.tw>
2025-09-24 08:22:42 +0800

    Delete comments of old regex.

--- src/rewrite.ts
+++ src/rewrite.ts
@@ -16,7 +16,6 @@ export function rewriteResourceLinks(base, resources, outerHTML) {
   // base decoded, no trailing '/' EXCEPT root /
   for (const { url, path: filePath } of resources) {
     const pathEscaped = escapeHTML(filePath);
-    //if (url.startsWith("http")) { // url must start with http
     const urlObj = new URL(url);
     // remove leading /
     const absPath = urlObj.pathname;
@@ -32,97 +31,14 @@ export function rewriteResourceLinks(base, resources, outerHTML) {
       outerHTML = outerHTML
         .replace(urlRegex(target, true), pathEscaped)
         .replace(urlRegex(target, false), filePath);
-      /*
-        .replace(
-          new RegExp(
-            `(['"])\\s*${escapeStringRegexp(escapeHTML(target))}\\s*\\1`,
-            "g"
-          ),
-          `"${pathEscaped}"`
-        )
-        .replace(
-          new RegExp(
-            `(\\&quot;|\\&apos;|\\&#34;|\\&#39;|\\&#x22;|\\&#x27;)\\s*${escapeStringRegexp(
-              escapeHTML(target)
-            )}\\s*\\1`,
-            "g"
-          ),
-          `&quot;${pathEscaped}&quot;`
-        )
-        .replace(
-          // style css url(/path) without quote
-          new RegExp(`\\(\\s*${escapeStringRegexp(target)}\\s*\\)`, "g"),
-          `(${path})`
-        )
-        .replace(
-          // style css url(/path) without quote
-          new RegExp(
-            `\\(\\s*${escapeStringRegexp(escapeHTML(target))}\\s*\\)`,
-            "g"
-          ),
-          `(${pathEscaped})`
-        )
-        .replace(
-          // in <style> tag, no html escape
-          // TODO: different escape format? ex: amp
-          new RegExp(`(['"])\\s*${escapeStringRegexp(target)}\\s*\\1`, "g"),
-          `"${path}"`
-        );
-        */
     }
-    //}
     // replace path-only uri
-    // ./ or / or without are the same, all base origin
     const relPathUriDecoded = decodeURI(relPathUri);
     const prefixRegexStr = `(\\.\\/|${base === "/" ? "" : base}\\/)?`;
     for (const target of [relPathUri, relPathUriDecoded]) {
       outerHTML = outerHTML
         .replace(urlRegex(target, true, prefixRegexStr), pathEscaped)
         .replace(urlRegex(target, false, prefixRegexStr), filePath);
-      /*
-        .replace(
-          new RegExp(
-            `(['"])\\s*(\.\/?)?${escapeStringRegexp(
-              escapeHTML(target)
-            )}\\s*\\1`,
-            "g"
-          ),
-          `"${pathEscaped}"`
-        )
-        .replace(
-          // inlined css with url quote
-          new RegExp(
-            `(\\&quot;|\\&apos;|\\&#34;|\\&#39;|\\&#x22;|\\&#x27;)\\s*(\.\/?)?${escapeStringRegexp(
-              escapeHTML(target)
-            )}\\s*\\1`,
-            "g"
-          ),
-          `&quot;${pathEscaped}&quot;`
-        )
-        .replace(
-          // style css url(/path) without quote
-          new RegExp(
-            `\\(\\s*(\.\/?)?${escapeStringRegexp(target)}\\s*\\)`,
-            "g"
-          ),
-          `(${path})`
-        )
-        .replace(
-          // style css url(/path) without quote
-          new RegExp(
-            `\\(\\s*(\.\/?)?${escapeStringRegexp(escapeHTML(target))}\\s*\\)`,
-            "g"
-          ),
-          `(${pathEscaped})`
-        )
-        .replace(
-          new RegExp(
-            `(['"])\\s*(\.\/?)?${escapeStringRegexp(target)}\\s*\\1`,
-            "g"
-          ),
-          `"${path}"`
-        );
-        */
     }
   }
   return outerHTML;
