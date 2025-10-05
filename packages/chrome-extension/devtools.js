54e3ce6b9e8f7ad8595daf913fba1d3e5a0abecb
Bill Tsui <b10902118@ntu.edu.tw>
2025-10-05 18:45:40 +0800

    [ext] Rename to Export.

--- packages/chrome-extension/devtools.js
+++ packages/chrome-extension/devtools.js
@@ -1,7 +1,4 @@
-chrome.devtools.panels.elements.createSidebarPane(
-  "My Sidebar",
-  function (sidebar) {
-    // sidebar initialization code here
-    sidebar.setPage("sidebar.html");
-  },
-);
+chrome.devtools.panels.elements.createSidebarPane("Export", function (sidebar) {
+  // sidebar initialization code here
+  sidebar.setPage("sidebar.html");
+});
